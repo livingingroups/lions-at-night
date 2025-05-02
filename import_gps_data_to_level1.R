ec8d7e17bf76dd53e26230d808955da1a7c7134d
Ariana Strandburg-Peshkin <ari@kn-eas-m019.local>
2025-05-02 15:46:22 +0300

    renamed again to level 1, also updated plotting and animation script to loop over all days

---
+++ import_gps_data_to_level1.R
@@ -0,0 +1,54 @@
+#Import high-res GPS data from csvs and convert to matrix form, preprocess to level 1
+
+library(cocomo)
+library(lubridate)
+Sys.setenv(TZ='UTC')
+
+#Directories
+datadir <- '~/Dropbox/lions_at_night/data_namibia/rawdata/'
+outdir <- '~/Dropbox/lions_at_night/data_namibia/processed/'
+level0_outfile <- paste0(outdir, 'lion_omukutu_latlon_xy_level0.RData')
+level1_outfile <- paste0(outdir, 'lion_omukutu_xy_level1.RData')
+
+#Read in data
+alldat <- data.frame()
+files <- list.files(datadir, full.names = T)
+for(i in 1:length(files)){
+  newdat <- read.csv(files[i],stringsAsFactors = F, header=T)
+  alldat <- rbind(alldat, newdat)
+}
+
+#rename to correct column names
+#timestamps are UTC
+colnames(alldat) <- c('individual.local.identifier','Timestamp','location.lat','location.long','height')
+
+#convert timestamp to POSIXct
+alldat$timestamp <- lubridate::as_datetime(alldat$Timestamp, tz = 'UTC', format = '%d/%m/%Y %H:%M:%OS')
+
+#make data chunks data frame
+data_chunks <- data.frame(start = min(alldat$timestamp), end = max(alldat$timestamp))
+
+#convert to matrix form and save output file
+cocomo::reformat_movebank_to_matrix(movebank_data = alldat, 
+                                    output_file_path = level0_outfile, 
+                                    data_chunks = data_chunks, 
+                                    utm_zone = 33, 
+                                    hemisphere = 'south', 
+                                    output_utm = T, 
+                                    output_latlon = T)
+
+#load the level 0 file we just created
+load(level0_outfile)
+
+#preprocess from level 0 to level 1
+out <- preprocess_gps_level0_to_level1(input_file_path = level0_outfile, 
+                                output_file_path = level1_outfile, 
+                                xs = xs, 
+                                ys = ys, 
+                                timestamps = timestamps, 
+                                ids = ids, 
+                                remove_unrealistic_speeds = T, 
+                                remove_isolated_points = T, 
+                                remove_unrealistic_locations = T, 
+                                interpolate_small_gaps = T, 
+                                interpolate_stationary_periods = T)
--- trajectory_plots.R
+++ trajectory_plots.R
@@ -2,41 +2,58 @@
 
 load('~/Dropbox/lions_at_night/data_namibia/processed/lion_omukutu_xy_level1.RData')
 
-
-#times to plot
-day <- 2
+#PARAMS
 step <- 60
+output_dir <- '~/Desktop/lion_viz/'
 
-t0 <- 24*60*60*day - 12*60*60 + 1
-tf <- t0 + 24*60*60
+#dirs
+output_dir_anim <- paste0(output_dir, 'anim/')
+output_dir_static <- paste0(output_dir, 'static/')
 
+n_days <- length(unique(lubridate::date(timestamps)))
+for(day in 1:n_days){
+  print(paste('day',day))
+  t0 <- 24*60*60*day - 12*60*60 + 1
+  tf <- t0 + 24*60*60
+  
+  #--- STATIC PLOT ---
+  print('generating plot')
+  n_inds <- nrow(xs)
+  ind_cols <- c('red','orange','darkgreen','blue','purple','magenta')
+  
+  xmin <- min(xs[,t0:tf], na.rm=T)
+  xmax <- max(xs[,t0:tf], na.rm=T)
+  ymin <- min(ys[,t0:tf], na.rm=T)
+  ymax <- max(ys[,t0:tf], na.rm=T)
+  
+  png(filename = paste0(output_dir_static, 'omukutu_day',day,'.png'), width = 800, height = 800, units = 'px')
+  
+  plot(NULL, xlim=c(xmin, xmax), ylim = c(ymin, ymax),asp=1,xlab='Easting (m)',ylab = 'Northing (m)')
+  verts <- seq(round(xmin, digits = -3)-5000, round(xmax, digits = -3)+5000, 1000)
+  horizs <- seq(round(ymin, digits = -3)-5000, round(ymax, digits = -3)+5000, 1000)
+  abline(h=horizs, col = '#00000011',lwd=2)
+  abline(v=verts, col = '#00000011',lwd=2)
+  for(i in 1:n_inds){
+    points(xs[i,seq(t0,tf,step)],ys[i,seq(t0,tf,step)],col=viridis(length(seq(t0,tf,step))),pch=19,cex=1)
+    lines(xs[i,seq(t0,tf)],ys[i,seq(t0,tf)],col = ind_cols[i],lwd=1)
+  }
+  dev.off()
+  
+  #---ANIMATION---
 
-n_inds <- nrow(xs)
-ind_cols <- c('red','orange','darkgreen','blue','purple','black')
-
-xmin <- min(xs[,t0:tf], na.rm=T)
-xmax <- max(xs[,t0:tf], na.rm=T)
-ymin <- min(ys[,t0:tf], na.rm=T)
-ymax <- max(ys[,t0:tf], na.rm=T)
-
-plot(NULL, xlim=c(xmin, xmax), ylim = c(ymin, ymax),asp=1)
-for(i in 1:n_inds){
-  points(xs[i,seq(t0,tf,step)],ys[i,seq(t0,tf,step)],col=viridis(length(seq(t0,tf,step))),pch=19,cex=1)
-  lines(xs[i,seq(t0,tf)],ys[i,seq(t0,tf)],col = ind_cols[i],lwd=1)
+  cocomo::generate_movement_and_calls_visualization(xs=xs, ys=ys, timestamps=timestamps, start_time = t0, end_time = tf, 
+                                                    time_step = step, 
+                                                    output_dir = '~/Desktop/lion_viz/anim/',
+                                                    tail_time=step*30,
+                                                    calls=data.frame(), 
+                                                    show_legend_calls = F, 
+                                                    show_legend_inds = T,
+                                                    ind_names = ids$code,
+                                                    scalebar_size = 1000,
+                                                    show_time = T,
+                                                    ind_point_size = 1.5)
+  dir <- paste0(output_dir_anim,'seq_',t0,'-',tf,'/')
+  system(command = paste('cd', dir))
+  system(command = paste0('ffmpeg -framerate 40 -i %d.png -r 16 -c:v libx264 -pix_fmt yuvj420p omukutu_viz_night_',day,'.mp4'))
+  system(command = paste0('mv omukutu_viz_night_',day,'.mp4 ../omukutu_viz_night_', day, '.mp4'))
 }
-
-#Number of inds tracked over time
-n_tracked <- colSums(!is.na(xs))
-plot(timestamps, n_tracked)
-
-dir.create(paste0('~/Desktop/lion_viz/day',day))
-cocomo::generate_movement_and_calls_visualization(xs=xs, ys=ys, timestamps=timestamps, start_time = t0, end_time = tf, 
-                                                  time_step = step, 
-                                                  output_dir = paste0('~/Desktop/lion_viz/day',day),
-                                                  tail_time=step*30,
-                                                  calls=data.frame(), 
-                                                  show_legend_calls = F, 
-                                                  show_legend_inds = T,
-                                                  ind_names = ids$code,
-                                                  scalebar_size = 1000,
-                                                  show_time = T)
