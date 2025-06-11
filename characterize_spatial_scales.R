a5e3bd5aa1abadaa1d7273ab0bce190e7f818ae3
Ariana Strandburg-Peshkin <ari@kn-eas-m019.local>
2025-06-11 11:23:29 +0200

    cleaned up documentation a bit

--- characterize_spatial_scales.R
+++ characterize_spatial_scales.R
@@ -1,4 +1,11 @@
-#What are the relevant spatial scales associated with lion groups?
+#What is lion cohesion + coordination like during the night?
+#What are the relevant spatial scales associated with lion coordination?
+#This script so far has 2 parts:
+#Part 1 looks at spatial scales of coordination by plotting various dyadic 
+#coordination-related metrics (e.g. heading correlation, change in dyadic distance, etc.)
+#as a function of distance between pairs.
+#Part 2 makes some more basic descriptive plots of speed, spread, polarization, etc. 
+#and their relationship with time of day.
 
 library(cocomo)
 library(lubridate)
@@ -9,10 +16,10 @@ library(lubridate)
 dir <- '~/Dropbox/lions_at_night/' #path to project directory
 R <- 10 #radius (in meters) for spatial headings
 speed_dt <- 10 #time difference to use for computing speeds and headings (in units of timesteps)
-min_speed_to_compute_heading <- .5 #in m/s
-start_times <- as.POSIXct(paste(seq.Date(date('2023-05-06'),date('2023-06-15'),by=1), '16:00:00'), tz = 'UTC') #timestamps of times to start each analysis period
-end_times <- as.POSIXct(paste(seq.Date(date('2023-05-07'),date('2023-06-16'),by=1), '06:00:00'), tz = 'UTC') #timestamps of times to end each analysis period
-dist_bins <- c(0, 10^seq(0.6,4,.4))
+min_speed_to_compute_heading <- .5 #minimum speed needed to have a heading (otherwise headings are set to NA)
+start_times <- as.POSIXct(paste(seq.Date(date('2023-05-06'),date('2023-06-15'),by=1), '16:00:00'), tz = 'UTC') #timestamps of times to start each analysis period for spatial scales analyses
+end_times <- as.POSIXct(paste(seq.Date(date('2023-05-07'),date('2023-06-16'),by=1), '06:00:00'), tz = 'UTC') #timestamps of times to end each analysis period for spatial scales analyses
+dist_bins <- c(0, 10^seq(0.6,4,.4)) #distance between dyads bins to use for spatial scales analyses
 
 #------------------------------------------------
 #LIBRARY
@@ -23,7 +30,7 @@ Sys.setenv(TZ='UTC')
 
 #FUNCTIONS
 
-#helper function to get statistics of the distribution of values
+#helper function to get relevant statistics of the distribution of values for a set of bins (binned by another variable x)
 #vals = values for which you are getting the distribution of (e.g. heading correlation)
 #x = what you are binning on (e.g. dyadic distance)
 #bins = bins to use for binning x
@@ -50,7 +57,7 @@ get_distrib_statistics <- function(vals, x, bins){
   return(out)
 }
 
-#helper function to make plot
+#helper function to make plots of different metrics
 make_plot <- function(dat, plotpath, ylab, ylim = NULL, xlab = 'Distance apart (m)', abline_y = NULL, logx = T, plot_means = T, plot_medians = T, plot_IQR = T){
   if(is.null(ylim)){
     ylim <- c(min(dat$q25,na.rm=T), max(dat$q75, na.rm=T))
@@ -79,7 +86,9 @@ make_plot <- function(dat, plotpath, ylab, ylim = NULL, xlab = 'Distance apart (
   dev.off()
 }
 
-#LOAD DATA
+#----LOAD AND PROCESS DATA----
+
+#Load data
 gpsfile <- paste0(dir, 'data_namibia/processed/lion_omukutu_xy_level1.RData')
 load(gpsfile) 
 
@@ -87,12 +96,12 @@ load(gpsfile)
 n_inds <- nrow(xs)
 n_times <- ncol(xs)
 
-#GET PERIODS OF DATA TO USE
+#Get periods of data to use for spatial scales analyses
 periods <- data.frame(start_time = start_times, end_time = end_times)
 periods$t0_idx <- match(periods$start_time, timestamps)
 periods$tf_idx <- match(periods$end_time, timestamps)
 
-#relevant time indexes
+#relevant time indexes for spatial scales analyses
 relevant_t_idxs <- c()
 for(i in 1:nrow(periods)){
   relevant_t_idxs <- c(relevant_t_idxs, periods$t0_idx[i]:periods$tf_idx[i])
@@ -108,22 +117,27 @@ for(i in 1:n_inds){
   out <- cocomo::get_heading_and_speed_temporal(xs[i, ], ys[i,], t_window = speed_dt, forward = T)
   heads[i,] <- out$heads
   speeds[i,] <- out$speeds
+  
+  #remove heading when speed is too low
   heads[i,which(speeds[i,] < min_speed_to_compute_heading)] <- NA
 }
-
-#remove heading when speed is too low
 
 #dyadic distances
 dyad_dists <- cocomo::get_group_dyadic_distances(xs, ys)
 
-#group polarization
+#group polarization (computed for all instances where at least 5 individuals were tracked)
 polarization <- cocomo::get_group_polarization(xs, ys, heading_type = 'temporal', t_window = speed_dt, min_inds_tracked = n_inds-1)
 
-#group speed
+#group speed (computed for all instances where at least 5 individuals were tracked)
 out <- cocomo::get_group_heading_and_speed(xs, ys, heading_type = 'temporal', t_window = speed_dt, min_inds_tracked = n_inds-1)
 group_speed <- out$speeds
 
-#dyadic distance changes, speed differences, and heading correlations between individuals
+#get mean dyadic distance (computed for all instances where at least 5 individuals were tracked)
+mean_dyad_dist <- apply(dyad_dists, 3, FUN = mean, na.rm=T)
+n_tracked <- colSums(!is.na(xs))
+mean_dyad_dist[which(n_tracked < n_inds - 1)] <- NA
+
+#dyadic distance changes, speed differences, and heading correlations between pairs of individuals
 dyad_dist_changes <- head_corrs <- speed_corrs <- speed_diffs <- log_speed_diffs <- array(NA, dim = c(n_inds,n_inds,n_times))
 for(i in 1:(n_inds-1)){
   for(j in (i+1):n_inds){
@@ -163,64 +177,54 @@ metrics$ang_between_heads <- get_distrib_statistics(acos(head_corrs_night)*180/p
 
 #-----PLOTS-----
 
-#----METRICS VS DISTANCE BETWEEN DYADS----
+#----Part 1: Spatial scales analyses----
 
-#Plot 1: Distribution of log(dyadic distances) between individuals
-png(filename = paste0(dir, 'plots/overall_hists/hist_log_dyad_dist.png'), width = 8, height = 6, units = 'in', res = 300)
-histo <- hist(log(dyad_dists, 10), plot=T, breaks=80, xlab = 'Dyadic distance (m) - log bins',main='', xlim = c(0,4), freq = F, xaxt ='n')
-axis(1, at = seq(0,4,1), labels = 10^seq(0,4,1))
-dev.off()
-
-#Plot 2: Angle between headings 
+#Plot: Angle between headings 
 plotpath <- paste0(dir, 'plots/spatial_scales/ang_between_heads.png')
 ylab <- "Angle between headings (degrees)"
 ylim <- c(0,180)
 dat <- metrics$ang_between_heads
 make_plot(dat, plotpath, ylab, ylim, abline_y = 90)
 
-#Plot 3: Heading correlation
+#Plot: Heading correlation
 plotpath <- paste0(dir, 'plots/spatial_scales/heading_corr.png')
 ylab <- "Heading correlation"
 ylim <- c(-1,1)
 dat <- metrics$head_corr
 make_plot(dat, plotpath, ylab, ylim, abline_y = 0)
 
-#Plot 4: Mean difference in speed as a function of distance 
+#Plot: Mean difference in speed as a function of distance 
 plotpath <- paste0(dir, 'plots/spatial_scales/abs_speed_diff.png')
 ylab = 'Absolute speed difference (m/s)'
 dat <- metrics$speed_diff
 make_plot(dat, plotpath, ylab)
 
-#Plot 5: Change in dyadic distance
+#Plot: Change in dyadic distance
 plotpath <- paste0(dir, 'plots/spatial_scales/dyad_dist_change.png')
 ylab = 'Change in dyadic distance (m)'
 dat <- metrics$dyad_dist_change
 make_plot(dat, plotpath, ylab, abline_y = 0)
 
-#Plot 6: Probability of approaching - needs a different approach to error bars etc
+#Plot: Probability of approaching - needs a different approach to error bars etc
 #plotpath <- paste0(dir, 'plots/spatial_scales/p_approach.png')
 #ylab = 'Probability of approach'
 #dat <- metrics$approach
 #make_plot(dat, plotpath, ylab, ylim = c(0.4,0.6),abline_y = 0.5)
 
-#----BASIC INFO DURING THE NIGHT----
-mean_dyad_dist <- apply(dyad_dists, 3, FUN = mean, na.rm=T)
+#----Part 2: Basic info during the night----
+
 hour_of_day <- lubridate::hour(timestamps)
-
-
-
-
-# hour_of_day_array <- array(NA, dim = dim(head_corrs))
-# for(i in 1:n_inds){
-#   for(j in 1:n_inds){
-#     hour_of_day_array[i,j,] <- hour_of_day
-#   }
-# }
 
 #Get metrics by hour
 metrics_by_hour <- list()
 metrics_by_hour$mean_dyad_dist <- get_distrib_statistics(mean_dyad_dist, hour_of_day, seq(-.5,24,1))
 metrics_by_hour$polarization <- get_distrib_statistics(polarization, hour_of_day, seq(-.5,24,1))
+
+#Plot: Distribution of log(dyadic distances) between individuals
+png(filename = paste0(dir, 'plots/overall_hists/hist_log_dyad_dist.png'), width = 8, height = 6, units = 'in', res = 300)
+histo <- hist(log(dyad_dists, 10), plot=T, breaks=80, xlab = 'Dyadic distance (m) - log bins',main='', xlim = c(0,4), freq = F, xaxt ='n')
+axis(1, at = seq(0,4,1), labels = 10^seq(0,4,1))
+dev.off()
 
 #Plot: Mean dyadic distance by hour
 plotpath <- paste0(dir, 'plots/hourly_metrics/mean_dyad_dist_by_hr.png')
@@ -254,7 +258,7 @@ text(4.3, .5, 'Person running', srt = 90)
 legend('bottomleft', legend = c('Individual','Group centroid'), col = c('red','black'),lwd=c(3,3))
 dev.off()
 
-#Speed distribution as a function of time of day (individual)
+#Plot: Speed distribution as a function of time of day (individual)
 png(filename = paste0(dir, 'plots/hourly_metrics/speed_distrib_by_hr.png'), width = 8, height = 6, units = 'in', res = 300)
 speedx <- seq(0,max(speeds,na.rm=T)+1,.001)
 plot(NULL, xlim = c(.1,10), ylim = c(0,1), log = 'x', xlab = 'Individual speed (m/s)', ylab = 'Cumulative probability')
@@ -270,7 +274,7 @@ labs <- paste0(hour_bins[1:(length(hour_bins)-1)],'-', hour_bins[2:length(hour_b
 legend('bottomright', legend = labs, col = cols, lwd = 2)
 dev.off()
 
-#Speed distribution as a function of time of day (group)
+#Plot: Speed distribution as a function of time of day (group)
 png(filename = paste0(dir, 'plots/hourly_metrics/centr_speed_distrib_by_hr.png'), width = 8, height = 6, units = 'in', res = 300)
 speedx <- seq(0,max(speeds,na.rm=T)+1,.001)
 plot(NULL, xlim = c(.1,10), ylim = c(0,1), log = 'x', xlab = 'Centroid speed (m/s)', ylab = 'Cumulative probability')
@@ -285,7 +289,6 @@ for(hour in 1:(length(hour_bins)-1)){
 labs <- paste0(hour_bins[1:(length(hour_bins)-1)],'-', hour_bins[2:length(hour_bins)],' UTC')
 legend('bottomright', legend = labs, col = cols, lwd = 2)
 dev.off()
-
 
 #Plot: histogram of group polarization - only for group speeds > 0.5 m/s
 #group_speed_bins <- seq(0,.5,1,2,3)
